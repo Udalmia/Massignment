feca0e2029a88ad6c3636080b7f34ed32b2569da
Udalmia <87439434+Udalmia@users.noreply.github.com>
2021-12-12 13:37:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 const express= require("express")
 const users= require("./users.json")
 const app=express();
-
+app.use(express.json());
+const logger=(req, res, next) => {
+    req.name = "Dhaval Chedda";
+    console.log(req.method);
+    next();
+};
+app.use(logger);
 app.get("/",(req, res) =>{
-    res.send("Welcome to Home page");
-});
-app.get("/users",(req, res) =>{
+    
     res.send(users);
 });
+app.post("/",(req,res) =>{
+    // console.log(req.body);
+     const newUsers=[...users, req.body]
+     res.send(newUsers);
+     //console.log(newUsers)
+ });
+ app.get("/:id",(req, res) =>{
+    const newUsers=[...users, req.params.id]
+    res.send(newUsers);
+   // console.log(req.params.id)
+   // res.send(users);
+});
+app.patch("/:book_name" ,(req, res) =>{
+    const newUsers=users.map((user) =>{
+        if(req.params.book_name === user.book_name){
+            return req.body;
+        }
+        return user;
+    });
+    res.send(newUsers);
+   // res.send("api_requested_by:", req.name)
+    console.log("api_requested_by:", req.name)
 
-app.listen(2346, function(){
-    console.log("listening to port 2346");
+});
+app.delete("/:book_name",(req, res) =>{
+    const newUsers =users.filter((user) => user.book_name !== req.params.book_name);
+    res.send(newUsers);
+});
+app.listen(2345, function(){
+    console.log("listening to port 2345");
 });
